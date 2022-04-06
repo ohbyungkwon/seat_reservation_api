a62c3e1ff86a13f951593476cb045bfb07214da5
LEE SUWON <ounmywww@naver.com>
2022-04-06 21:00:29 +0900

    TEST COMMIT

--- src/main/java/com/seat/reservation/domain/Seat.java
+++ src/main/java/com/seat/reservation/domain/Seat.java
@@ -1,11 +1,14 @@
 package com.seat.reservation.domain;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
 @Entity
+@NoArgsConstructor
 public class Seat {
     private Long id;
 
