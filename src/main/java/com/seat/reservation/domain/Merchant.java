6569d82c482a5646fe6e6bbea3029b0858f6fee5
92keon@gmail.com <92keon@gmail.com>
2022-04-09 14:17:19 +0900

    merchant

--- src/main/java/com/seat/reservation/domain/Merchant.java
+++ src/main/java/com/seat/reservation/domain/Merchant.java
@@ -1,20 +1,42 @@
 package com.seat.reservation.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.*;
+import javax.persistence.*;
+import java.util.Date;
 
 @Table
 @Entity
 public class Merchant {
-    private Long id;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     @Id
-    public Long getId() {
-        return id;
-    }
+    private int merchant_reg_number; // 사업자 등록 번호
+
+    @JoinColumn
+    //@OneToMany(fetch = FetchType.LAZY)
+    private int user_id; // 자리 등록한 유저
+
+    private String rep_phone; // 가맹점 사장님 번호
+
+    private String rep_name; // 가맹점 사장님 이름
+
+    private String merchant_tel; // 가맹점 번호
+
+    private String merchant_name; // 가맹점 상호
+
+    private String category; // 업종 카테고리 ex. Pc방, cafe
+
+    private String register_flag; // 등록 및 해지여부
+
+    private String address; // 가맹점 주소
+
+    private String zipcode; // 가맹점 우편번호
+
+    private Date register_date;  // 가맹점 등록일자
+
+    private Date modify_date; // 가맹점 수정일자
+
+
+
+
+
 }
