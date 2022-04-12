9934da95e8ed79c023e677ba0c3ed6b9924a696c
LEE SUWON <ounmywww@naver.com>
2022-04-12 21:18:55 +0900

    /* 22.04.09 추가 사항 */ 1. enum 추가 (RegisterCode) 2. SeatHistory에 Change User 추가 3. 컬럼별 주석 추가

--- src/main/java/com/seat/reservation/domain/SeatHistory.java
+++ src/main/java/com/seat/reservation/domain/SeatHistory.java
@@ -1,7 +1,9 @@
 package com.seat.reservation.domain;
 
+import com.seat.reservation.domain.enums.RegisterCode;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -11,16 +13,21 @@ import java.util.Date;
 @Getter
 @Builder
 @AllArgsConstructor
+@EntityListeners(value = AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SeatHistory {
     @Id
     @CreatedDate
-    private Date registerDate;
+    private Date registerDate; // 좌석을 등록한 날짜
 
     @Id
     @JoinColumn
     @ManyToOne(fetch = FetchType.LAZY)
-    private Seat seat;
+    private Seat seat; // 등록한 좌석
 
-    private char registerCode;
+    @Enumerated(EnumType.STRING)
+    private RegisterCode registerCode; // 등록 코드
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user; // 좌석을 등록한 유저
 }
