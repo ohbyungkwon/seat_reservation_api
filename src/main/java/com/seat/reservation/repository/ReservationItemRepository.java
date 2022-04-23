68faa8e0c7b12e6de2562d933d1691b997b5a31b
오병권 <ohbg123@naver.com>
2022-04-23 13:53:13 +0900

    Reservation Repository 초안 커밋

--- src/main/java/com/seat/reservation/repository/ReservationItemRepository.java
+++ src/main/java/com/seat/reservation/repository/ReservationItemRepository.java
@@ -1,7 +1,12 @@
 package com.seat.reservation.repository;
 
+import com.seat.reservation.domain.Reservation;
 import com.seat.reservation.domain.ReservationItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationItemRepository extends JpaRepository<ReservationItem, Long> {
+    // 추후 QueryDsl로 변경 예정
+    List<ReservationItem> findByReservation(Reservation reservation);
 }
