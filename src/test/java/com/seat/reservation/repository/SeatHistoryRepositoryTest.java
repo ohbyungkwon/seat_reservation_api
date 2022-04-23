8bb8fe0eabcac630edce813e877b5db28b35446d
LEE SUWON <ounmywww@naver.com>
2022-04-23 17:28:27 +0900

    1. Login History '-' -> '_' 수정
    2. MerchantHistory
    Sequence 이름 수정
    
    3. SeatHistory
    Sequence 이름 수정

--- src/test/java/com/seat/reservation/repository/SeatHistoryRepositoryTest.java
+++ src/test/java/com/seat/reservation/repository/SeatHistoryRepositoryTest.java
@@ -1,13 +1,11 @@
 package com.seat.reservation.repository;
 
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.Assert.*;
 
+@SpringBootTest
 public class SeatHistoryRepositoryTest {
-    @Test
-    void insertSeatTest(){
-
-    }
 
 }
