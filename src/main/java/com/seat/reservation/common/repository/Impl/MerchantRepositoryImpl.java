12835d318967952cc751e835c7a5f957c864b1ff
92keon@gmail.com <92keon@gmail.com>
2022-06-11 14:44:52 +0900

    MerchantRepositoryTest 및 UpzongDto 변경

--- src/main/java/com/seat/reservation/common/repository/Impl/MerchantRepositoryImpl.java
+++ src/main/java/com/seat/reservation/common/repository/Impl/MerchantRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.seat.reservation.common.repository.Impl;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.seat.reservation.common.domain.Merchant;
 import com.seat.reservation.common.dto.MerchantDto;
 import com.seat.reservation.common.dto.QMerchantDto_show;
 import com.seat.reservation.common.repository.custom.MerchantRepositoryCustom;
@@ -27,30 +28,20 @@ import static com.seat.reservation.common.domain.QUpzong.upzong;
      *  WHERE 1 = 1
      *  AND 업종 = 업종
      *  AND 지역 = 지역
-     *  AND 날짜 = 날짜
-     *  AND 인원 = 인원
+     *  AND 상호 = 상호
      */
 
     @Override
-    public List<MerchantDto.show> findMerchant(Integer merchantRegNum) {
-        return jpaQueryFactory.select(
-                    new QMerchantDto_show(
-                            merchant.merchantRegNum,
-                            merchant.repPhone,
-                            merchant.repName,
-                            merchant.merchantTel,
-                            merchant.merchantName,
-                            merchant.upzong.id,
-                            merchant.address,
-                            merchant.zipCode
-                            )
-                )
-                .from(merchant)
-                .join(merchant.upzong, upzong).fetchJoin()
-                .where(eqMerchantRegNum(merchantRegNum))
+    public List<Merchant> findMerchant(Integer merchantRegNum) {
+        return jpaQueryFactory.selectFrom(merchant)
+                .join(merchant.upzong, upzong).fetchJoin() // 업종 조인
+                .where(eqMerchantName(String.valueOf(merchant.merchantName))) // 상호 = 상호
                 .fetch();
     }
 
+    public BooleanExpression eqMerchantName(String merchantName){
+        return StringUtils.isEmpty(merchantName) ? null : merchant.merchantName.eq(merchantName);
+    }
     public BooleanExpression eqMerchantRegNum(Integer merchantRegNum){
         return StringUtils.isEmpty(merchantRegNum) ? null : merchant.merchantRegNum.eq(merchantRegNum);
     }
--- src/main/java/com/seat/reservation/common/repository/custom/MerchantRepositoryCustom.java
+++ src/main/java/com/seat/reservation/common/repository/custom/MerchantRepositoryCustom.java
@@ -1,11 +1,12 @@
 package com.seat.reservation.common.repository.custom;
 
+import com.seat.reservation.common.domain.Merchant;
 import com.seat.reservation.common.dto.MerchantDto;
 
 import java.util.List;
 
 public interface MerchantRepositoryCustom {
 
-    List<MerchantDto.show> findMerchant(Integer merchantRegNum);
+    List<Merchant> findMerchant(Integer merchantRegNum);
 
 }
