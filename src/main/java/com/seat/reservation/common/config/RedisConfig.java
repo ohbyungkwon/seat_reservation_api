9e9943fd55c5d07b5d22c275281443692c5e66a0
오병권 <ohbg123@naver.com>
2023-06-26 11:10:50 +0900

    .

--- src/main/java/com/seat/reservation/common/config/RedisConfig.java
+++ src/main/java/com/seat/reservation/common/config/RedisConfig.java
@@ -1,11 +1,13 @@
 package com.seat.reservation.common.config;
 
 import com.seat.reservation.common.cache.CustomRedisCache;
+import com.seat.reservation.common.cache.CustomRedisCacheManager;
 import com.seat.reservation.common.cache.CustomRedisCacheWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -17,10 +19,18 @@ import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
 public class RedisConfig {
     public static final String DEFAULT_REDIS_STORAGE = "reservation-redis-storage";
+
+    @Value("${spring.redis.caches}")
+    private String caches;
 
     @Value("${spring.redis.host}")
     private String host;
@@ -59,6 +69,16 @@ public class RedisConfig {
     }
 
     @Bean
+    public RedisCacheManager redisCacheManager() {
+        Set<String> cacheSet = Arrays.stream(caches.split(",")).collect(Collectors.toSet());
+        return RedisCacheManager.RedisCacheManagerBuilder.fromCacheWriter(redisCacheWriter())
+                .cacheDefaults(redisCacheConfiguration())
+                .disableCreateOnMissingCache()
+                .initialCacheNames(cacheSet)
+                .build();
+    }
+
+    @Bean
     public CustomRedisCacheWriter redisCacheWriter(){
         return new CustomRedisCacheWriter(redisConnectionFactory());
     }
@@ -70,8 +90,11 @@ public class RedisConfig {
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
 
+    /**
+     * Default로 지정된 Cache를 사용할 경우 바로 DI 가능하도록 구성
+     */
     @Bean
-    public CustomRedisCache redisCacheManager(){
+    public CustomRedisCache redisCache(){
         return new CustomRedisCache(DEFAULT_REDIS_STORAGE, redisCacheWriter(), redisCacheConfiguration());
     }
 }
